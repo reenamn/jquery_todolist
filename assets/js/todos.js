968959f515f3595b659d68d42818ee730d2921fd
reenamn <reenavar@gmail.com>
2018-12-26 11:44:39 -0800

    basic functionality completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,36 @@
-// alert("connected!");
+// Check off to dos by clicking
+$('ul').on("click", "li",function(){
+
+	$(this).toggleClass('completed');
+});
+
+$('ul').on("click", "span",function(event){
+	console.log("clicked X");
+
+	event.stopPropagation();
+
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+})
+
+$("input[type='text']").keypress(function(){
+// console.log(event.key);
+
+	//You clicked Enter
+	if(event.key === "Enter")
+	{
+		//grab text from imput
+		var todoText = $(this).val();
+
+		//create new LI
+
+		$("ul").append("<li><span>X</span> "+todoText+"</li>");
+
+		//clear the input field
+		$(this).val("");
+	}
+
+
+
+})
